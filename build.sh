a6a7c4237579c406a6aa34327074271f77e391c9
gitsagat <git@sagat.com.br>
2023-08-10 13:51:01 -0300

    Ajustes

--- build.sh
+++ build.sh
@@ -8,8 +8,17 @@ wget https://dev.mysql.com/get/mysql-apt-config_0.8.12-1_all.deb
 sudo dpkg -i mysql-apt-config_0.8.12-1_all.deb
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 467B942D3A79BD29
 sudo apt install -f mysql-client=5.7* mysql-community-server=5.7* mysql-server=5.7* -y
+echo "Press y|Y for Yes, any other key for No: [y]"
+echo "Please enter 0 = LOW, 1 = MEDIUM and 2 = STRONG: [1]"
+echo "Estimated strength of the password: [25]"
+echo "Remove anonymous users? (Press y|Y for Yes, any other key for No) : [y]"
+echo "Disallow root login remotely? (Press y|Y for Yes, any other key for No) : [y]"
+echo "Remove test database and access to it? (Press y|Y for Yes, any other key for No) : [y]"
+echo "Reload privilege tables now? (Press y|Y for Yes, any other key for No) : [y]"
+
 sudo mysql_secure_installation
-echo "criar o usuário CREATE USER 'aredecrm'@'%' IDENTIFIED BY 'MinhaSenha'"
+
+echo "mysql -u root -p criar o usuário CREATE USER 'aredecrm'@'%' IDENTIFIED BY 'MinhaSenha'"
 echo "add my.cnf [mysqld]"
 echo "max_allowed_packet=32M"
 echo "bind-address    = 0.0.0.0"
