4efa6423d7fb89db8c73c3babcdacc935dbf0fab
gitsagat <git@sagat.com.br>
2023-08-10 11:57:36 -0300

    Ajustes

--- build.sh
+++ build.sh
@@ -8,3 +8,4 @@ wget https://dev.mysql.com/get/mysql-apt-config_0.8.12-1_all.deb
 sudo dpkg -i mysql-apt-config_0.8.12-1_all.deb
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 467B942D3A79BD29
 sudo apt install -f mysql-client=5.7* mysql-community-server=5.7* mysql-server=5.7* -y
+sudo mysql_secure_installation
