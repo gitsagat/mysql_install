2ec1af4b31f27f9a834ceaf0766e31ffc0af206e
gitsagat <git@sagat.com.br>
2023-08-10 11:27:14 -0300

    Ajustes

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo "Install MySQL5.7.40..."
 
 #Install MySQL
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.12-1_all.deb
-dpkg -i mysql-apt-config_0.8.12-1_all.deb
+sudo dpkg -i mysql-apt-config_0.8.12-1_all.deb
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 467B942D3A79BD29
 sudo apt update -y
